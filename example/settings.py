a95883208ff31a6ad7c176c2471e92098c142c91
Pratik7778 <pratik.hadkhale@berrybytes.com>
2024-08-05 14:28:24 +0545

    Update settings.py

--- example/settings.py
+++ example/settings.py
@@ -87,6 +87,15 @@ DATABASES = {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
+
+    ‘default’: {
+      ‘ENGINE’: ‘django.db.backends.postgresql’,
+      ‘NAME’: ‘postgres’,
+      ‘USER’: ‘postgres’,
+      ‘PASSWORD’: ‘Pratik@123’,
+      ‘HOST’: ‘localhost’,
+      ‘PORT’: ‘5432’, 
+      }
 }
 
 
